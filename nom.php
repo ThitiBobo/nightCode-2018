b36ecf91302ad9f0783969938300b315178b3f8b
Lakraa Omer <omer.lakraa@etud.u-picardie.fr>
2018-01-18 22:36:24 +0100

    Envoi du JSON

--- nom.php
+++ nom.php
@@ -1,19 +1,7 @@
-<?php 
-$tab = ['team'=>'Les guignoles de\'info'];
-
-$content_tab = json_encode($tab);
-
-var_dump($content_tab);
-
-// Nom du fichier à créer
-$fic = 'nom.json';
-
-// Ouverture du fichier
-$fic = fopen($fic, 'w+');
-
-// Ecriture dans le fichier
-fwrite($fic, $content_tab);
-
-// Fermeture du fichier
-fclose($fic);
+<?php
+    header('content-type:application/json; charset: UTF-8');
+    
+   
+    echo json_encode([ 'name' => 'La team' ]);
+    
 ?>
