2783f3dd8f42808474950469d7a4270dca5a9444
SAVARY-DERELLE Victor <victor.savary-derelle@etud.u-picardie.fr>
2018-01-19 02:25:53 +0100

    test du post

--- showtimes.php
+++ showtimes.php
@@ -1,6 +1,8 @@
 <?php
     header('content-type:application/json; charset: UTF-8');
-    
+
+
+if ($_SERVER['REQUEST_METHOD']=='GET'){
     if(!isset($_GET['id'])){
         $array = [
             ['id' => 1, "name" => "star wars"],
@@ -10,6 +12,10 @@
     }else{
         echo json_encode(['id' => $_GET['id'], "name" => "star wars"]);
     }
+  }
+else if ($_SERVER['REQUEST_METHOD']=='POST')
+{
+  echo "post";
+}
 
-    
 ?>
